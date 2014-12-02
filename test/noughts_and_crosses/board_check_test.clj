7b92ed26b0361d85fe073dcbbf7c4361a1175a7f
Joseph Watkins <joseph.o.watkins@gmail.com>
2014-12-02 17:27:53 +0000

    Clean up tests and move to appropriate ns.

---
+++ test/noughts_and_crosses/board_check_test.clj
@@ -0,0 +1,75 @@
+(ns noughts-and-crosses.board-check_test
+  (:require [clojure.test :refer :all]
+            [noughts-and-crosses.board-check :refer :all]))
+
+
+; define some boards
+(def empty-board [[:- :- :-]
+                  [:- :- :-]
+                  [:- :- :-]])
+
+(def full-board [[:O :X :O]
+                 [:X :X :O]
+                 [:O :O :X]])
+
+(def legal-for-x-to-move-next [[:O :X :O]
+                               [:- :- :-]
+                               [:- :- :-]])
+
+(def legal-for-o-to-move-next [[:X :- :-]
+                               [:O :- :-]
+                               [:X :- :-]])
+
+(deftest x-and-o-move-legal
+  (is (= (legal-move-available? empty-board :X) true))
+  (is (= (legal-move-available? empty-board :O) true)))
+
+(deftest no-legal-moves
+  (is (= (legal-move-available? full-board :X) false))
+  (is (= (legal-move-available? full-board :O) false)))
+
+(deftest x-can-go?
+  (is (= (legal-move-available? legal-for-x-to-move-next :X) true))
+  (is (= (legal-move-available? legal-for-x-to-move-next :O) false)))
+
+(deftest o-can-go?
+  (is (= (legal-move-available? legal-for-o-to-move-next :O) true))
+  (is (= (legal-move-available? legal-for-o-to-move-next :X) false))
+  )
+
+; define some boards with illegal numbers of rows or columns.
+(def wrong-cols [[:X]
+                 [:- :- :-]
+                 [:O :O :-]])
+
+(def wrong-rows [[] [] [] [] []])
+
+(deftest check-number-of-columns
+  (is (= (board-has-n-cols? wrong-cols 3) false))
+  (is (= (board-has-n-cols? full-board 3) true))
+  )
+
+
+(deftest check-number-of-rows
+  (is (= (board-has-n-rows? wrong-rows 3) false))
+  (is (= (board-has-n-rows? full-board 3) true))
+  )
+
+
+; define board with illegal differience between number of X and O's.
+(def illegal-board [[:X :X :-]
+                    [:- :X :-]
+                    [:- :O :-]])
+
+(deftest illegal-move-finder
+  (is (= (board-legal? illegal-board) false))
+  (is (= (board-legal? empty-board) true))
+  (is (= (board-legal? legal-for-o-to-move-next) true))
+  (is (= (board-legal? legal-for-x-to-move-next) true))
+  )
+
+(def nested-str-vec [["1" "2" "3"] ["4" "5" "6"]])
+(def nested-key-vec [[:1 :2 :3] [:4 :5 :6]])
+
+(deftest convert-vector
+  (is (= nested-key-vec (map-nested-vector str->keyword nested-str-vec))))
--- test/noughts_and_crosses/core_test.clj
+++ test/noughts_and_crosses/core_test.clj
@@ -2,52 +2,27 @@
   (:require [clojure.test :refer :all]
             [noughts-and-crosses.core :refer :all]))
 
-(def empty-board [[:- :- :-]
-                  [:- :- :-]
-                  [:- :- :-]])
 
-(def full-board [[:O :X :O]
-                 [:X :X :O]
-                 [:O :O :X]])
-
-(def legal-for-x [[:O :X :O]
-                  [:- :- :-]
-                  [:- :- :-]])
-
-(def legal-for-o [[:X :- :-]
+(def legal-for-x [[:X :- :-]
                   [:O :- :-]
-                  [:X :- :-]])
+                  [:O :- :-]])
 
-(deftest x-and-o-move-legal
-  (is (= (move-legal? :X empty-board) true))
-  (is (= (move-legal? :O empty-board) true)))
+(def legal-for-o [[:O :- :O]
+                  [:X :- :-]
+                  [:X :X :-]])
 
-(deftest no-legal-moves
-  (is (= (move-legal? :X full-board) false))
-  (is (= (move-legal? :O full-board) false)))
 
-(deftest x-can-go?
-  (is (= (move-legal? :X legal-for-x) true))
-  (is (= (move-legal? :O legal-for-x) false)))
+(def map-legal-x {:board legal-for-x :piece :X})
+(def map-illegal-x {:board legal-for-o :piece :X})
 
-(deftest o-can-go?
-  (is (= (move-legal? :O legal-for-o) true))
-  (is (= (move-legal? :X legal-for-o) false))
+(def map-legal-o {:board legal-for-o :piece :O})
+(def map-illegal-o {:board legal-for-x :piece :O})
+
+
+(deftest test-process-map
+  (is (= (:legal (process-map map-legal-x)) true))
+  (is (= (:legal (process-map map-illegal-x)) false))
+  (is (= (:legal (process-map map-legal-o)) true))
+  (is (= (:legal (process-map map-illegal-o)) false))
   )
 
-(def illegal-board [[:X :X :-]
-                    [:- :X :-]
-                    [:- :O :-]])
-
-(deftest illegal-move-finder
-  (is (= (legal-board? illegal-board) false))
-  (is (= (legal-board? empty-board) true))
-  (is (= (legal-board? legal-for-o) true))
-  (is (= (legal-board? legal-for-x) true))
-  )
-
-(def nested-str-vec [["1" "2" "3"] ["4" "5" "6"]])
-(def nested-key-vec [[:1 :2 :3] [:4 :5 :6]])
-
-(deftest convert-vector
-  (is (= nested-key-vec (map-nested-vector str->keyword nested-str-vec))))
