ea39dc20491f26b041680c6707dac449ddb84ff4
Joseph Watkins <joseph.o.watkins@gmail.com>
2014-12-03 20:03:39 +0000

    Up date deps.

--- project.clj
+++ project.clj
@@ -4,7 +4,13 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/data.json "0.2.5"]]
+                 [org.clojure/data.json "0.2.5"]
+                 [ring/ring-core "1.3.1"]
+                 [ring/ring-jetty-adapter "1.3.1"]
+                 [ring/ring-json "0.3.1"]
+                 [compojure "1.2.2"]]
+  :plugins [[lein-ring "0.8.13"]]
+  :ring {:handler noughts-and-crosses.server/main-routes}
   :main ^:skip-aot noughts-and-crosses.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
