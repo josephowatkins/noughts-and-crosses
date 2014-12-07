1bd3c1be3d97c764fd8fe7d6a3e9b8ccc76a91f2
Joseph Watkins <joseph.o.watkins@gmail.com>
2014-12-07 13:48:42 +0000

    Update tests - check that minmax successfully counters a winning situation.

--- test/noughts_and_crosses/minmax_test.clj
+++ test/noughts_and_crosses/minmax_test.clj
@@ -2,64 +2,117 @@
   (:require [clojure.test :refer :all]
             [noughts-and-crosses.minmax :refer :all]))
 
+;; Define boards, their associated empty spaces and next boards.
+
 (def empty-board [["-" "-" "-"]
                   ["-" "-" "-"]
                   ["-" "-" "-"]])
-(def empty-board-moves (for [x (range 3) y (range 3)] [x y]))
+
+(def empty-board-available-spaces (for [x (range 3) y (range 3)] [x y]))
 
 
 (def full-board [["O" "X" "O"]
                  ["X" "X" "O"]
                  ["O" "O" "X"]])
-(def full-board-moves '())
 
-(def legal-for-x-to-move-next [["O" "X" "O"]
-                               ["-" "-" "-"]
-                               ["-" "-" "-"]])
-(def moves-for-x (for [x '(1 2) y '(0 1 2)] [x y]))
-(def next-moves-for-x '([["O" "X" "O"]
-                         ["X" "-" "-"]
-                         ["-" "-" "-"]]
+(def full-board-available-spaces '())
 
-                        [["O" "X" "O"]
-                         ["-" "X" "-"]
-                         ["-" "-" "-"]]
 
-                        [["O" "X" "O"]
-                         ["-" "-" "X"]
-                         ["-" "-" "-"]]
+(def x-to-move-next [["O" "X" "O"]
+                     ["-" "-" "-"]
+                     ["-" "-" "-"]])
 
-                        [["O" "X" "O"]
-                         ["-" "-" "-"]
-                         ["X" "-" "-"]]
+(def x-to-move-next-available-spaces (for [x '(1 2) y '(0 1 2)] [x y]))
 
-                        [["O" "X" "O"]
-                         ["-" "-" "-"]
-                         ["-" "X" "-"]]
+(def x-to-move-next-possible-boards '([["O" "X" "O"]
+                                       ["X" "-" "-"]
+                                       ["-" "-" "-"]]
 
-                        [["O" "X" "O"]
-                         ["-" "-" "-"]
-                         ["-" "-" "X"]]))
+                                       [["O" "X" "O"]
+                                        ["-" "X" "-"]
+                                        ["-" "-" "-"]]
 
-(def legal-for-o-to-move-next [["X" "-" "-"]
-                               ["O" "-" "-"]
-                               ["X" "-" "-"]])
-(def moves-for-o (for [x '(0 1 2) y '(1 2)] [x y]))
+                                       [["O" "X" "O"]
+                                        ["-" "-" "X"]
+                                        ["-" "-" "-"]]
+
+                                       [["O" "X" "O"]
+                                        ["-" "-" "-"]
+                                        ["X" "-" "-"]]
+
+                                       [["O" "X" "O"]
+                                        ["-" "-" "-"]
+                                        ["-" "X" "-"]]
+
+                                       [["O" "X" "O"]
+                                        ["-" "-" "-"]
+                                        ["-" "-" "X"]]))
+
+
+(def o-to-move-next [["X" "-" "-"]
+                     ["O" "-" "-"]
+                     ["X" "-" "-"]])
+
+(def o-to-move-next-available-spaces (for [x '(0 1 2) y '(1 2)] [x y]))
+
+(def o-to-move-next-possible-boards '([["X" "O" "-"]
+                                       ["O" "-" "-"]
+                                       ["X" "-" "-"]]
+
+                                       [["X" "-" "O"]
+                                        ["O" "-" "-"]
+                                        ["X" "-" "-"]]
+
+                                       [["X" "-" "-"]
+                                        ["O" "O" "-"]
+                                        ["X" "-" "-"]]
+
+                                       [["X" "-" "-"]
+                                        ["O" "-" "O"]
+                                        ["X" "-" "-"]]
+
+                                       [["X" "-" "-"]
+                                        ["O" "-" "-"]
+                                        ["X" "O" "-"]]
+
+                                       [["X" "-" "-"]
+                                        ["O" "-" "-"]
+                                        ["X" "-" "O"]]))
 
 
 (deftest check-get-availble-moves
-  (is (= (get-availible-moves empty-board) empty-board-moves))
-  (is (= (get-availible-moves full-board) full-board-moves))
-  (is (= (get-availible-moves legal-for-x-to-move-next) moves-for-x))
-  (is (= (get-availible-moves legal-for-o-to-move-next) moves-for-o))
+  (is (= (get-availible-moves empty-board) empty-board-available-spaces))
+  (is (= (get-availible-moves full-board) full-board-available-spaces))
+  (is (= (get-availible-moves x-to-move-next) x-to-move-next-available-spaces))
+  (is (= (get-availible-moves o-to-move-next) o-to-move-next-available-spaces))
   )
 
 (deftest check-next-moves
-  (is (= (get-next-boards legal-for-x-to-move-next "X") next-moves-for-x))
+  (is (= (get-next-boards x-to-move-next "X") x-to-move-next-possible-boards))
+  (is (= (get-next-boards o-to-move-next "O") o-to-move-next-possible-boards))
   )
 
-(def nearly-full-board [["-" "X" "O"]
-                        ["-" "O" "X"]
-                        ["-" "-" "-"]])
 
-(print (return-next-board nearly-full-board "X"))
+;; Difficult to test minmax but it must block a potential win
+; Define boards and appropriate counters
+
+(def o-win-in-one [["-" "X" "O"]
+                   ["-" "O" "X"]
+                   ["-" "-" "-"]])
+
+(def counter-o-win-in-one [["-" "X" "O"]
+                           ["-" "O" "X"]
+                           ["X" "-" "-"]])
+
+(def x-win-in-one [["O" "X" "O"]
+                   ["-" "X" "-"]
+                   ["-" "-" "-"]])
+
+(def counter-x-win-in-one [["O" "X" "O"]
+                           ["-" "X" "-"]
+                           ["-" "O" "-"]])
+
+(deftest minmax-must-block-opponent-win
+  (is (= counter-o-win-in-one (generate-next-board o-win-in-one "X")))
+  (is (= counter-x-win-in-one (generate-next-board x-win-in-one "O")))
+  )
