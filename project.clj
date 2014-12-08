256c00df6726a0aa3410bede4f3ebb7289b0a7bd
Joseph Watkins <joseph.o.watkins@gmail.com>
2014-12-08 10:50:49 +0000

    Updated dependencies and :init function path.

--- project.clj
+++ project.clj
@@ -8,9 +8,11 @@
                  [ring/ring-core "1.3.1"]
                  [ring/ring-jetty-adapter "1.3.1"]
                  [ring/ring-json "0.3.1"]
-                 [compojure "1.2.2"]]
+                 [compojure "1.2.2"]
+                 [com.taoensso/faraday "1.5.0" :exclusions [org.clojure/clojure]]]
   :plugins [[lein-ring "0.8.13"]]
-  :ring {:handler noughts-and-crosses.server/main-routes}
+  :ring {:handler noughts-and-crosses.core/main-routes
+         :init noughts-and-crosses.database/setup-tables}
   :main ^:skip-aot noughts-and-crosses.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
